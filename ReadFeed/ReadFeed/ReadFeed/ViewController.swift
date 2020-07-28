b51d67668d6475504f2d7cfe81973c14c2902ca3
Carly Grizzaffi <47189340+carlygrizzaffi@users.noreply.github.com>
2020-07-28 14:20:30 -0400

    Basic Storyboard

---
+++ ReadFeed/ReadFeed/ReadFeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReadFeed
+//
+//  Created by Carly Grizzaffi on 7/28/20.
+//  Copyright © 2020 KWK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
